b05437a182bb255542033762f500ab44a831474a
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-31 14:53:59 -0400

    Chapter 1 - Assignment 1 Done: Print CS git add -A

---
+++ chapter-1/CH1-assignment1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "* * * * * * * * * * * * * * * * * * * * * * *" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "        C C C          S S S S        !!" << endl;
+    cout << "      C       C      S          S     !!" << endl;
+    cout << "     C              S                 !!" << endl;
+    cout << "    C                 S               !!" << endl;
+    cout << "    C                  S S S S        !!" << endl;
+    cout << "    C                           S     !!" << endl;
+    cout << "     C                           S    !!" << endl;
+    cout << "      C       C      S          S       " << endl;
+    cout << "        C C C          S S S S        00" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * *" << endl;
+    cout << "" << endl;
+    cout << "   Computer Science is Cool Stuff!!!" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+
+    return 0;
+}
