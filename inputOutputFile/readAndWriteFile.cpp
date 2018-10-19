15d794875fbd83cc7ef3f3e39050befa78d7cfa5
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-10-18 22:57:05 -0400

    Input Output files using fstream, ifstream, and ofstream

---
+++ inputOutputFile/readAndWriteFile.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream in_stream;
+
+    in_stream.open("numbers.txt");
+
+    if (in_stream.fail())
+    {
+        cerr << "Cannot open file";
+        exit(1);
+    }
+
+    int x, y, z;
+    string a, b, c, d;
+
+    in_stream >> x >> y >> z >> a >> b >> c >> d;
+
+    cout << x << " " << y << " " << z << " " << a << " " << b << " " << c << " " << d << endl;
+
+    in_stream.close();
+
+    ofstream outFile;
+    outFile.open("doc.txt");
+
+    outFile << "Sample writing data to an empty document" << endl;
+
+    outFile.close();
+
+    return 0;
+}
